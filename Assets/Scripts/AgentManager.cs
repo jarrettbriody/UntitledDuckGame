71477f43c64e198ff30ecd24b76fd07d084e0c3f
Nicholas Bazos <nbazos15@gmail.com>
2020-04-13 21:44:40 -0400

    Weapon System Foundation
    Hitscan shooting, crosshair, bullet trails, muzzle particle effect, FPS camera with gun placed
    
    Current caveat: Bullet trails offset for some reason

--- Assets/Scripts/AgentManager.cs
+++ Assets/Scripts/AgentManager.cs
@@ -10,11 +10,13 @@ public class AgentManager : MonoBehaviour
 
     public Text ducksLeft;
 
+    public Text ammoLeft;
+
     public List<GameObject> ducks;
 
     public Terrain terrain;
 
-    public GameObject cow;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
@@ -25,10 +27,11 @@ public class AgentManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        hp.text = "HP: " + cow.GetComponent<Cow>().hp;
+        hp.text = "HP: " + player.GetComponent<Player>().playerHealth;
         ducksLeft.text = "Ducks Remaining: " + ducks.Count;
+        ammoLeft.text = "Ammo: " + player.GetComponent<Player>().gunAmmoRemaining;
 
-        if(ducks.Count <= 1)
+        if (ducks.Count <= 1)
             SceneManager.LoadScene("scene");
     }
 }
---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    float bulletSpeed = 1000f;
+    [HideInInspector] public Vector3 raycastHitPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += GameObject.FindGameObjectWithTag("MainCamera").transform.forward * Time.deltaTime * bulletSpeed;
+    }
+}
